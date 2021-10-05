817138368e8f4522a1d40afeefbe752bf543aa79
andrei_malashenkov <mALAaNDRdMIT2016>
2021-10-05 11:35:45 +0300

    ДЗ 6

--- 06_PrimitiveDataTypes/IntOrInteger/src/FindCharRu.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/FindCharRu.java
@@ -1,7 +1,7 @@
 public class FindCharRu {
     public static void main(String[] args) {
         for (int i = 0; i <= 65536; i++) {
-            if ((i >= (int) 'а' && i <= (int) 'я') || (i >= (int) 'А' && i <= (int) 'Я') || (char) i == 'Ё') {
+            if ((i >= (int) 'а' && i <= (int) 'я') || (i >= (int) 'А' && i <= (int) 'Я') || (char) i == 'Ё' || (char) i == 'ё') {
                 System.out.println("code = " + i + ", char = " + (char) i);
             }
         }
